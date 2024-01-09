e3cb452ef5dc86081c2833e24d4c2ce76a0600a2
Victor Martins Torres <victotorres@gmail.com>
2024-01-09 10:36:54 -0300

    Projeto inicial

--- src/main/java/com/example/cardapio/CardapioApplication.java
+++ src/main/java/com/example/cardapio/CardapioApplication.java
@@ -1,5 +1,6 @@
 package com.example.cardapio;
 
+import com.example.cardapio.controller.FoodController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,8 @@ public class CardapioApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CardapioApplication.class, args);
+
+		FoodController foodController = new FoodController();
 	}
 
 }
